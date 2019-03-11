d913e5d0e544c7f4b09794e296d86b24cce7bc5e
Fraukje <fraukjecoopmans@live.nl>
2019-03-11 11:00:44 +0100

    Add minor changes (draw game, diagonal win, plot tweaks)

--- tic_tac_toe_game.R
+++ tic_tac_toe_game.R
@@ -1,3 +1,4 @@
+# Function that get player's move from user input
 get_player_move <- function (player, board) {
         prompt_message = paste("Player", player, ", choose your move (1-9): ", sep = " " )
         n <- suppressWarnings(as.integer(readline(prompt=prompt_message)))
@@ -9,18 +10,22 @@ get_player_move <- function (player, board) {
         return(n)
 }
 
+# Function that checks if player wins
 check_winner <- function(board, player){
         if(any(apply(board, 1, function(x, want) isTRUE(all.equal(x, want)), c(player, player, player))) |
-           any(apply(board, 2, function(x, want) isTRUE(all.equal(x, want)), c(player, player, player)))) {
+           any(apply(board, 2, function(x, want) isTRUE(all.equal(x, want)), c(player, player, player))) |
+           all(c(board[1], board[5], board[9]) == c(player, player, player)) |
+           all(c(board[3], board[5], board[6]) == c(player, player, player))) {
                   return(paste("Player", player, "has three in a row and wins!"))
         }      
         else{ 
                   return(NULL)}
 }
 
+# Tic tac toe game function
 tic_tac_toe <- function() {
         
-        library(raster) ##Visualizing the board needs raster package
+        library(raster) #Visualizing the board needs raster package
   
         print("Welcome to Tic, Tac, Toe game!")
   
@@ -30,21 +35,27 @@ tic_tac_toe <- function() {
         player <- sample(1:2, 1)
         print(paste("Player", player, "may start", sep = " "))
         
-        ## Start playing: get player move, update and show board.
+        # Start playing: get player move, update and show board.
         while(turn < 10) {
                 n <- get_player_move(player, board)
                 board[n] <- player
-                plot(raster(board), legend = "false")
-                turn <- turn + 1
+                plot(raster(board), legend = FALSE, axes = FALSE, 
+                     main="Tic Tac Toe game")
 
-                ## Check if game is done
+                # Check if game is done
                 result <- check_winner(board, player)
                 if (!is.null(result)) {
                           print(result)
                           break
                 }
-                ## Swap player
+                # Swap player
                 player <- if(player == 1){2}
                 else {1}
+                
+                turn <- turn + 1
+        }
+        # If there is no result after 9 moves, game is tied.
+        if (is.null(result)) {
+                print("Game tie")
         }
 }
